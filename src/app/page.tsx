29480453787a348e35361dfa9b58176a28d562e8
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-01-25 18:09:36 +0100

    Update next.config.js and tailwind.config.js, and modify Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Link from 'next/link';
+import Button from './components/button';
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-xl">Home page</h1>
+    <main className="flex flex-col items-center mx-40 my-40 ">
+      <h1 className="text-3xl font-semibold text-gray-900 mb-4">
+        Welcome to Our Website
+      </h1>
+      <p className="text-center text-gray-600 mb-4">
+        Explore companies based on categories, sales, representation in
+        different countries, and more. Create your own narrative for what makes
+        our platform special!
+      </p>
+      <Link href="/dashboard">
+        <Button className="btn btn-primary mt-4">Go To Dashboard</Button>
+      </Link>
     </main>
   );
 }
