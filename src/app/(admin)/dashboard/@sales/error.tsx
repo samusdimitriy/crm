ee45c935c73ef7710a20181bd562e8dea1ee09b4
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-01-25 08:21:30 +0100

    Update background color and remove unused components

--- src/app/(admin)/dashboard/@sales/error.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import React from 'react';
-
-export interface ErrorComponentProps {}
-
-export default function ErrorComponent({}: ErrorComponentProps) {
-  return <div>Unexpected error inside slot sales</div>;
-}
--- src/app/(admin)/dashboard/@sales/loading.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export interface LoadingProps {}
-
-export default function Loading({}: LoadingProps) {
-  return <div>Loading...</div>;
-}
